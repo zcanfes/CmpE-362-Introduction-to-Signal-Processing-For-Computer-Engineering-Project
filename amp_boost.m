7c2701fac0e8ffccfc8b321f0bbd3bae2311a5fe
dogukan <dogukanakarc@gmail.com>
2022-06-14 15:54:30 +0300

    seperated function and main

---
+++ amp_boost.m
@@ -0,0 +1,23 @@
+
+function modulated_wave = amp_boost(y, fs, boost)
+    num_channel = size(y, 2);
+
+    for i = 1:num_channel
+        y_channel = y(:, i).';
+
+        l=length(y_channel);
+        t = (1:l)/fs; % Find the time interval to plot
+        
+        AM = boost .* ones(1, l);
+
+        if i == 1
+            modulated_wave = AM .* y_channel;
+        else
+            wave = AM .* y_channel;
+            modulated_wave = vertcat(modulated_wave, wave);
+        end
+    end
+    modulated_wave = modulated_wave.';
+end
+
+
